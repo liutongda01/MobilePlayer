8105609be4c87b38d7e4014bcc294bc8d300912a
liutongda01 <1823594184@qq.com>
2017-05-19 10:49:08 +0800

    Welcome欢迎页面

--- app/src/main/java/svntest/mobileplayer/WelcomeActivity.java
+++ app/src/main/java/svntest/mobileplayer/WelcomeActivity.java
@@ -1,13 +1,43 @@
 package svntest.mobileplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void startMainActivity() {
+        //1进入主页面
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        //2关闭当前页面
+        finish();
+     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        startMainActivity();
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        //移除所有消息
+        handler.removeCallbacksAndMessages(null);
+        super.onDestroy();
     }
 }
