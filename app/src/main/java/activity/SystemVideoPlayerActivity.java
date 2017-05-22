dfc8d550490b6e4a992830bf1b6e8accffbb8b5d
liutongda01 <1823594184@qq.com>
2017-05-22 19:59:43 +0800

    监听按手机声音音键改变声音

--- app/src/main/java/activity/SystemVideoPlayerActivity.java
+++ app/src/main/java/activity/SystemVideoPlayerActivity.java
@@ -15,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -480,6 +481,24 @@ public class SystemVideoPlayerActivity extends AppCompatActivity implements View
             receiver = null;
         }
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            currentVoice--;
+            updateVoiceProgress(currentVoice);
+            handler.removeMessages(HIDE_MEDIACONTROLLER);
+            handler.sendEmptyMessageDelayed(HIDE_MEDIACONTROLLER,4000);
+            return true;
+        }else if(keyCode == KeyEvent.KEYCODE_VOLUME_UP){
+            currentVoice++;
+            updateVoiceProgress(currentVoice);
+            handler.removeMessages(HIDE_MEDIACONTROLLER);
+            handler.sendEmptyMessageDelayed(HIDE_MEDIACONTROLLER,4000);
+            return true;
+         }
+        return super.onKeyDown(keyCode, event);
     }
 }
 
