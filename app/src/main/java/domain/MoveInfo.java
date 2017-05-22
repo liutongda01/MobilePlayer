b068f05806ff22cf0d8e29fc211d6e7ef7d4edce
liutongda01 <1823594184@qq.com>
2017-05-23 01:49:30 +0800

    xUtils3请求网络数据

---
+++ app/src/main/java/domain/MoveInfo.java
@@ -0,0 +1,9 @@
+package domain;
+
+/**
+ * Created by Liutongda on 2017/5/23.
+ */
+
+public class MoveInfo {
+
+}
--- app/src/main/java/pager/NetVideoPager.java
+++ app/src/main/java/pager/NetVideoPager.java
@@ -1,32 +1,81 @@
 package pager;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
+
+import java.util.List;
+
+import adapter.NetVideoAdapter;
+import domain.MediaItem;
+import domain.MoveInfo;
 import fragment.BaseFragment;
+import svntest.mobileplayer.R;
 
 /**
  * Created by Liutongda on 2017/5/19.
  */
 
-public class NetVideoPager extends BaseFragment{
-    private TextView textView;
+public class NetVideoPager extends BaseFragment {
+    private NetVideoAdapter adapter;
+
+    private ListView lv;
+    private TextView tv_nodata;
 
     //重写视图
     @Override
     public View initView() {
-        textView = new TextView(context);
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(context, R.layout.fragment_net_video_pager, null);
+        lv = (ListView) view.findViewById(R.id.lv);
+        tv_nodata = (TextView) view.findViewById(R.id.tv_nodata);
+        return view;
+
+
+
     }
+
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("网络视频的内容");
+        getDataFromNet();
     }
+
+    private void getDataFromNet() {
+        final RequestParams request = new RequestParams("http://api.m.mtime.cn/PageSubArea/TrailerList.api");
+        x.http().get(request, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+
+
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.e("TAG", "xUtils联网失败==" + ex.getMessage());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+
+    }
+
 }
